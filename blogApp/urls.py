a931e22de8b53afa9ad7ff4f0d1c29592934714a
Asim Thaha <iamasimthaha@gmail.com>
2023-11-17 22:07:26 +0530

    added my post view and view all views

--- blogApp/urls.py
+++ blogApp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('add/', views.addView, name="add"),
     path('search/', views.searchView, name="search"),
     path('view/', views.displayView, name="view"),
+    path('viewMy/', views.displayMyView, name="viewMy"),
 ]
